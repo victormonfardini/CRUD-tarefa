469999fc351fb87e2ecdfa93ca218b38eb5b1964
Victor Monfardini <victor.monfardini@gmail.com>
2018-01-07 22:07:56 -0200

    Update database e tabelas.sql

--- banco/database e tabelas.sql
+++ banco/database e tabelas.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE sitetarefa;
 
-Create table usuarios (
+Create table usuarios(
 id Int UNSIGNED NOT NULL AUTO_INCREMENT,
 login Varchar(30),
 senha Varchar(40),
 Primary Key (id));
 
-Create table tarefa (
+Create table tarefa(
 id Int UNSIGNED NOT NULL AUTO_INCREMENT,
 nome Varchar(40),
 descricao text,
